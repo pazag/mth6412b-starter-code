fbae44b80c06513022b1bf1b975c9c78647987b7
pazag <43011966+pazag@users.noreply.github.com>
2018-09-14 16:41:22 -0400

    Update main_stsp.jl

--- projet/phase1/main_stsp.jl
+++ projet/phase1/main_stsp.jl
@@ -1,1 +1,16 @@
 
+function create_graph(filename::String)
+  graph=Graph(filename, Node{Number}[],Edge{Number}[])
+  node_dict,edge_list=read_stsp(filename)
+  for node_id in keys(node_dict)
+    node=Node(node_id,node_dict[node_id])
+    add_node!(graph,node)
+  edge_origin=1
+  for edge_brut in edge_list
+      node_1=Node(edge_origin,node_dict[edge_origin])
+      for edge_ in edge_brut
+         node2=Node(edge_[1],node_dict[edge_[1])
+         edge=Edge(e
+    
+  
+  
