3d1a3870847ed75698062e650139d3ec7115189f
pazag <43011966+pazag@users.noreply.github.com>
2018-09-19 14:24:38 -0400

    Update main_phase1.jl

--- projet/phase1/main_phase1.jl
+++ projet/phase1/main_phase1.jl
@@ -1,1 +1,11 @@
+include("node.jl")
+include("edge.jl")
+include("graph.jl")
+include("read_stsp.jl")
+include("create_graph.jl")
 
+
+path="../../instances/stsp/"
+filename="bays29.tsp"
+
+create_graph(path+filename)
